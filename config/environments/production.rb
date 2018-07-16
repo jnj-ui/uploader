6057e33d8b106c525017532ffcf62fe56d6c091e
shizuka takada <takada.a4t@gmail.com>
2018-07-16 06:27:23 +0000

    production.rbの記述を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,6 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'limitless-sierra-47143.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'limitless-sierra-47143' }
   config.action_mailer.delivery_method = :letter_opener
 end
